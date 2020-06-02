eb5ab18c9efc0250e2f58bc306311cf6dce70660
カワモトセイヤ <kawamotoseiya@kawamotoseiyanoMacBook-Pro.local>
2020-06-02 23:14:31 +0900

    [start]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'todolists/new'
+    get 'todolists/new'
 	get 'top' => 'homes#top'
-
 	post 'todolists' => 'todolists#create'
+
+
+   get 'todolists' => 'todolists#index'
 end
